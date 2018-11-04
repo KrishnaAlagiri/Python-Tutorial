0c35ad18f1332a955ac45154e8d093139d21f743
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2018-11-05 00:49:22 +0530

    Code Completed

--- copyspecial.py
+++ copyspecial.py
@@ -15,7 +15,31 @@ import subprocess
 # +++your code here+++
 # Write functions and modify main() to call them
 
+def get_path(directory):
+  result = []
+  paths = os.listdir(directory)
+  for filename in paths:
+    match = re.search(r'__(\w+)__', filename)
+    if match:
+      result.append(os.path.abspath(os.path.join(directory, filename)))
+  return (result)
 
+
+def Copy(paths, to_dir):
+  print("Poda")
+  if not os.path.exists(to_dir):
+    os.mkdir(to_dir)
+  for path in paths:
+    fname = os.path.basename(path)
+    new_p = os.path.join(to_dir, fname)
+    shutil.copy(path, new_p)
+
+def Zip(paths, zipfile):
+  print("Poda")
+  Copy(paths, zipfile)
+  print("Poda")
+  Command = ('powershell.exe Compress-Archive -LiteralPath '+ zipfile + ' -DestinationPath ' + zipfile  )
+  os.system(Command)
 
 def main():
   # This basic command line argument parsing code is provided.
@@ -25,7 +49,7 @@ def main():
   # which is the script itself.
   args = sys.argv[1:]
   if not args:
-    print("usage: [--todir dir][--tozip zipfile] dir [dir ...]");
+    print("usage: [--todir dir][--tozip zipfile] dir [dir ...]")
     sys.exit(1)
 
   # todir and tozip are either set from command line
@@ -47,6 +71,16 @@ def main():
 
   # +++your code here+++
   # Call your functions
-  
+  paths = []
+  for directory in args:
+    paths.extend(get_path(directory))
+
+  if todir:
+    Copy(paths, todir)
+  elif tozip:
+    Zip(paths, tozip)
+  else:
+    print('\n'.join(paths))
+
 if __name__ == "__main__":
   main()
