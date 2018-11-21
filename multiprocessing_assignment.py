c98fbe107fffc75ce684618457dd127008de6aed
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2018-11-21 12:27:10 +0530

    Initial Upload

---
+++ multiprocessing_assignment.py
@@ -0,0 +1,9 @@
+"""
+   Multiprocessing Assignment
+===================================
+Write a python program multiprocessing_assignment.py which calculates the
+square of all numbers 1..10 using a separate Process to calculate each number.
+Use a shared memory Array to store the results.
+(Remember to lock the shared array before manipulating it.)
+
+"""
