46a50bab58f00f999f4471c47ac3b474c0704045
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2018-12-06 21:23:12 +0530

    Finish Assignment

--- multiprocessing_assignment.py
+++ multiprocessing_assignment.py
@@ -7,3 +7,24 @@ Use a shared memory Array to store the results.
 (Remember to lock the shared array before manipulating it.)
 
 """
+import os
+from multiprocessing import Process, Lock
+lock = Lock()
+
+def square(number):
+    lock.acquire()
+    try:
+        result = number * number
+    finally:
+        lock.release()
+    proc = os.getpid()
+    print('{0} squared to {1} by process id: {2}'.format(number, result, proc))
+
+if __name__ == '__main__':
+    procs = []
+    for number in range(1,10):
+        proc = Process(target=square, args=(number,))
+        procs.append(proc)
+        proc.start()
+    for proc in procs:
+        proc.join()
